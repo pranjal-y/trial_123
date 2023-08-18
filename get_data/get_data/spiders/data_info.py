57d0fe6f5a777bda0f2800c65971597f4efcd921
pranjal-y <141224795+pranjal-y@users.noreply.github.com>
2023-08-18 23:45:34 +0530

    Update data_info.py

--- get_data/get_data/spiders/data_info.py
+++ get_data/get_data/spiders/data_info.py
@@ -8,18 +8,19 @@ class DataInfoSpider(scrapy.Spider):
     start_urls = []
 
     def __init__(self, url=None, tags=None, num_columns=None, column_headings=None, *args, **kwargs):
-        super(DataInfoSpider, self).__init__(*args, **kwargs)
-        if url:
-            self.start_urls = [url]
-            parsed_url = urlparse(url)
-            self.allowed_domains = [parsed_url.netloc]  # Extract and set the domain
+    super(DataInfoSpider, self).__init__(*args, **kwargs)
+    if url:
+        self.start_urls = [url]
+        parsed_url = urlparse(url)
+        self.allowed_domains = [parsed_url.netloc]  # Extract and set the domain
 
-         if tags is not None:
-            self.tags = tags.split(',')  # Convert to a list
-        else:
-            self.tags = []       
-        self.num_columns = int(num_columns)
-        self.column_headings = column_headings.split(',') if column_headings else []  # Convert to a list
+    if tags is not None:
+        self.tags = tags.split(',')  # Convert to a list
+    else:
+        self.tags = []
+        
+    self.num_columns = int(num_columns)
+    self.column_headings = column_headings.split(',') if column_headings else []  # Convert to a list
 
 
     def parse(self, response):
